40aa3db341fa8140a20e8b0b76bd6bcb21d26ac3
CatalinaDonic <katesponge@gmail.com>
2024-11-14 14:31:07 +0200

    assignment 2 upload

---
+++ rectangle.py
@@ -0,0 +1,6 @@
+width = float(input("Enter width of the rectangle:"))
+height = float(input("Enter height of the rectangle:"))
+area = width*height
+circumference = 2*width+2*height
+print("Area:", area)
+print("Circumference:", circumference)
